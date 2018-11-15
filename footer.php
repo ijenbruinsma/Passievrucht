e31d52d93fde0a29a4e062ae64b57188dcdb8cc9
ijenbruinsma <148796@student.aventus.nl>
2018-11-15 12:11:41 +0100

    test

---
+++ footer.php
@@ -0,0 +1,10 @@
+    <div class="footer-wrapper">
+        <div class="lower full-width">
+            <div class="container">
+                <div class="row">
+                    <h6 class="footerText">&copy <?php echo date('Y'); ?> Passievrucht</h6>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ header.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Passievrucht</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li><a href="index.php">Home</a></li>
+      <li><a href="portfolio.php">Portfolio</a></li>
+      <li><a href="overons.php">Over ons</a></li>
+      <li><a href="contact.php">Contact</a></li>
+    </ul>
+  </div>
+</nav>
