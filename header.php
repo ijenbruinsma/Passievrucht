8f9c2725a9080b79abb29274bb73730f6ef05201
ijenbruinsma <148796@student.aventus.nl>
2018-11-20 12:18:13 +0100

    nieuw template

--- header.php
+++
@@ -1,9 +0,0 @@
-<section class="success" id="about">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <h2>Content</h2>
-                </div>
-            </div> 
-        </div>
-</section>
