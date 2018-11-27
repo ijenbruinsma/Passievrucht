10c6ffe4444cf5691ba6397b07d18074e49d227c
ijenbruinsma <148796@student.aventus.nl>
2018-11-27 16:08:58 +0100

    index

--- index.php
+++ index.php
@@ -14,16 +14,9 @@
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="css/portfoliostyle.css" rel="stylesheet">
 
-
     <!-- Custom fonts for this template -->
-
-    
-   
-
     <link href="vendor/fontawesome-free-5.5.0-web/css/all.min.css" rel="stylesheet" type="text/css">
-    
     <link href='fonts/themix.css' rel='stylesheet' type='text/css'>
-
 
     <!-- Custom styles for this template -->
     <link href="css/agency.min.css" rel="stylesheet">
@@ -35,7 +28,6 @@
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
       <div class="container">
-<!--        <a class="navbar-brand js-scroll-trigger" href="#page-top">Passievrucht</a>-->
           <img src="img/aventuslogo.png" style="max-height: 70px;">
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           Menu
