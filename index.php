f7323c6d465266bbb44d58c24b76f7a796528625
ijenbruinsma <148796@student.aventus.nl>
2018-11-27 11:44:53 +0100

    index.php

--- index.php
+++ index.php
@@ -247,7 +247,7 @@
     </section>
 
     <!-- Team -->
-    <section class="bg-light" id="team">
+    <section id="team">
       <div class="container">
         <div class="row">
           <div class="col-lg-12 text-center">
@@ -259,7 +259,7 @@
             <div class="team-member">
               <img class="mx-auto rounded-circle" src="img/team/1.jpg" alt="">
               <h4>Kay Garland</h4>
-              <p class="text-muted">Lead Designer</p>
+              <p class="text-dark">Lead Designer</p>
               <ul class="list-inline social-buttons">
                 <li class="list-inline-item">
                   <a href="#">
@@ -283,7 +283,7 @@
             <div class="team-member">
               <img class="mx-auto rounded-circle" src="img/team/2.jpg" alt="">
               <h4>Larry Parker</h4>
-              <p class="text-muted">Lead Marketer</p>
+              <p class="text-dark">Lead Marketer</p>
               <ul class="list-inline social-buttons">
                 <li class="list-inline-item">
                   <a href="#">
@@ -307,7 +307,7 @@
             <div class="team-member">
               <img class="mx-auto rounded-circle" src="img/team/3.jpg" alt="">
               <h4>Diana Pertersen</h4>
-              <p class="text-muted">Lead Developer</p>
+              <p class="text-dark">Lead Developer</p>
               <ul class="list-inline social-buttons">
                 <li class="list-inline-item">
                   <a href="#">
@@ -330,7 +330,7 @@
         </div>
         <div class="row">
           <div class="col-lg-8 mx-auto text-center">
-            <p class="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p>
+            <p class="large text-dark">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p>
           </div>
         </div>
       </div>
