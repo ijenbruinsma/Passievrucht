89c9e5d3dd9f1e72631ca3556a5746801381ce47
ijenbruinsma <148796@student.aventus.nl>
2018-11-27 12:00:12 +0100

    zoekbalk met function

--- index.php
+++ index.php
@@ -73,9 +73,65 @@
     <section class="bg-light" id="portfolio">
       <div class="container">
         <div class="row">
+
           <div class="col-lg-12 text-center">
             <h2 class="section-heading text-uppercase">Portfolio</h2>
           </div>
+          <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Zoek naar projecten..." title="Zoek naar projecten">
+          <style>
+            * {
+              box-sizing: border-box;
+            }
+
+            #myInput {
+              background-image: url('/css/searchicon.png');
+              background-position: 10px 12px;
+              background-repeat: no-repeat;
+              width: 100%;
+              font-size: 16px;
+              padding: 12px 20px 12px 40px;
+              border: 1px solid #ddd;
+              margin-bottom: 12px;
+            }
+
+            #myUL {
+              list-style-type: none;
+              padding: 0;
+              margin: 0;
+            }
+
+            #myUL li a {
+              border: 1px solid #ddd;
+              margin-top: -1px; /* Prevent double borders */
+              background-color: #f6f6f6;
+              padding: 12px;
+              text-decoration: none;
+              font-size: 18px;
+              color: black;
+              display: block
+            }
+
+            #myUL li a:hover:not(.header) {
+              background-color: #eee;
+            }
+            </style>
+            <script>
+            function myFunction() {
+                var input, filter, ul, li, a, i;
+                input = document.getElementById("myInput");
+                filter = input.value.toUpperCase();
+                ul = document.getElementById("myUL");
+                li = ul.getElementsByTagName("li");
+                for (i = 0; i < li.length; i++) {
+                    a = li[i].getElementsByTagName("a")[0];
+                    if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                        li[i].style.display = "none";
+                    } else {
+                        li[i].style.display = "none";
+                    }
+                }
+            }
+            </script>
         </div>
         <div class="row">
           <div class="col-md-4 col-sm-6 portfolio-item">
