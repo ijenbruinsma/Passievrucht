ed207ee798ea808286a20b56bc382d6d43cf8e68
koenstaz <39735657+koenstaz69@users.noreply.github.com>
2018-11-28 15:50:09 +0100

    Create testj.php

---
+++ testj.php
@@ -0,0 +1,35 @@
+<?php
+
+$data = array(
+
+'project_title' => "1"
+);
+
+$url = "http://viggo.holidayrentcenter.com/project/get?" . http_build_query($data);
+CallAPI($url);
+
+
+
+function CallAPI($url, $data = false)
+{
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_POST, 1);
+    if ($data)
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+             
+
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+    $result = curl_exec($curl);
+     curl_close($curl);
+
+    $array = json_decode($result, true);
+    echo "<pre>";
+    print_r($array);
+    echo "</pre>";
+    return $result;
+}
+
+
+?>
