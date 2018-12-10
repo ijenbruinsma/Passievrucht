f141875b04d9317417e63ec86b2f11700847dcce
Bas Bluemink <148098@student.aventus.nl>
2018-12-10 09:08:30 +0100

    nieuwe versie

--- index.php
+++ index.php
@@ -85,31 +85,50 @@
         </div>
 
         <div class="portfolio-item">
-          <div class="item java">
-            <img src="img/about/1.jpg">
-          </div>
-
-          <div class="item java">
-            <img src="img/about/2.jpg">
-          </div>
-
-          <div class="item web">
-            <img src="img/about/4.jpg">
-          </div>
           
-          <div class="item web">
-            <img src="img/about/2.jpg">
-          </div>
-          
-          <div class="item hardsoftware">
-            <img src="img/about/3.jpg">
-          </div>
+<?php
+                    
 
-          <div class="item html">
-            <img src="img/about/4.jpg">
-          </div>
-        
-        </div>
+$data = array(
+
+'project_title' => "1"
+);
+
+$url = "http://viggo.holidayrentcenter.com/project/get?" . http_build_query($data);
+CallAPI($url);
+
+
+
+function CallAPI($url, $data = false)
+{
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_POST, 1);
+    if ($data)
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+             
+
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    $result =
+    $result = curl_exec($curl);
+     curl_close($curl);
+
+    $array = json_decode($result, true);
+    echo "<pre>";
+    print_r($array);
+    echo "</pre>";
+    return $result;
+} 
+?>		
+					
+                                
+                               
+                                 
+                          
+                        
+                    
+                    
+         </div>
 
 
 
