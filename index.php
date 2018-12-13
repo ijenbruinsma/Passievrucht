0ab5bce51fb1195dfadd50d29f5738ee97826a63
J_Aydin <janberd.aydin@gmail.com>
2018-12-13 10:49:46 +0100

    over ons aangepast

--- index.php
+++ index.php
@@ -283,31 +283,6 @@
           <div class="col-sm-4">
             <div class="team-member">
               <img class="mx-auto rounded-circle" src="img/team/1.jpg" alt="">
-              <h4>Bas Bluemink</h4>
-              <p class="text-dark">Api</p>
-              <ul class="list-inline social-buttons">
-                <li class="list-inline-item">
-                  <a href="#">
-                    <i class="fab fa-twitter"></i>
-                  </a>
-                </li>
-                <li class="list-inline-item">
-                  <a href="#">
-                    <i class="fab fa-facebook-f"></i>
-                  </a>
-                </li>
-                <li class="list-inline-item">
-                  <a href="#">
-                    <i class="fab fa-linkedin-in"></i>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-
-          <div class="col-sm-4">
-            <div class="team-member">
-              <img class="mx-auto rounded-circle" src="img/team/1.jpg" alt="">
               <h4>Koen van der Heijden</h4>
               <p class="text-dark">Website/Api</p>
               <ul class="list-inline social-buttons">
@@ -409,7 +384,7 @@
           <div class="col-sm-4">
             <div class="team-member">
               <img class="mx-auto rounded-circle" src="img/team/1.jpg" alt="">
-              <h4>Teun Ankesmit</h4>
+              <h4>Teun Ankersmit</h4>
               <p class="text-dark">Api</p>
               <ul class="list-inline social-buttons">
                 <li class="list-inline-item">
